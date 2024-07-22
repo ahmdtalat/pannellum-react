2ffd430ff0ce1f6ed70c16e404ec28abddb2d289
Ahmd <a.talaat@planradar.com>
2024-07-22 15:47:06 +0200

    wip: poc

--- demo/src/components/ImageDemo.js
+++ demo/src/components/ImageDemo.js
@@ -1,161 +1,313 @@
-import React, { Component } from "react";
+import React, { useRef } from "react";
 import { Pannellum } from "../../../src";
 import myImage from "../images/alma.jpg";
-import myImage2 from "../images/milan.jpg";
 
-export default class ImageDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mediaPhoto: myImage,
-      yaww: 180,
-      test: false,
-      updateText: "initial",
-      author: "author"
-    };
-    this.ref = React.createRef();
-  }
+export const ImageDemo = () => {
+  const ref = useRef();
+  const [state, setState] = React.useState({
+    mediaPhoto: myImage,
+    yaww: 180,
+    test: false,
+    updateText: "initial",
+  });
 
-  hanldeClickImage = (evt, args) => {
-    console.log(args.name);
-    this.setState({
-      mediaPhoto: myImage2
-    });
-  };
+  return (
+    <div className="image_main">
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <button onClick={() => setState((prevState) => ({ author: "change" }))}> change author </button>
+        <button onClick={() => setState((prevState) => ({ yaww: prevState.yaww + 10 }))}> change yaw </button>
+        <button onClick={() => setState({ updateText: "after update" })}> Should not update </button>
+        <h3>{state.updateText}</h3>
+        <button
+          onClick={() => {
+            setState({ test: true, yaww: 100, mediaPhoto: myImage });
+          }}
+        >
+          {" "}
+          enable{" "}
+        </button>
+        <div>{state.test && <button onClick={handleClick}> disable </button>}</div>
+      </div>
+      <h2 className="section_title">Image Component</h2>
+      <div className="pannellum_div">
+        <Pannellum
+          ref={ref}
+          width="800px"
+          height="400px"
+          image={state.mediaPhoto}
+          pitch={10}
+          yaw={state.yaww}
+          hfov={120}
+          autoLoad
+          author={state.author}
+          title=""
+        >
+          <Pannellum.Hotspot
+            type="info"
+            pitch={11}
+            yaw={-167}
+            text="Info Hotspot Text 3"
+            URL="https://github.com/farminf"
+          />
 
-  handleClick = () => {
-    this.setState({
-      mediaPhoto: myImage2,
-      test: false
-    });
-  };
+          <Pannellum.Hotspot
+            yaw={120}
+            pitch={212}
+            type="custom"
+            handleDrag={(evt, args) => {
+              const draggable = document.querySelector("#textInfo");
+              const container = document.querySelector(".pnlm-dragfix");
 
-  render() {
-    return (
-      <div className="image_main">
-        <div style={{ display: "flex", flexDirection: "row" }}>
-          <button
-            onClick={() => this.setState(prevState => ({ author: "change" }))}
-          >
-            {" "}
-            change author{" "}
-          </button>
-          <button
-            onClick={() =>
-              this.setState(prevState => ({ yaww: prevState.yaww + 10 }))
-            }
-          >
-            {" "}
-            change yaw{" "}
-          </button>
-          <button onClick={() => this.setState({ updateText: "after update" })}>
-            {" "}
-            Should not update{" "}
-          </button>
-          <h3>{this.state.updateText}</h3>
-          <button
-            onClick={() => {
-              this.setState({ test: true, yaww: 100, mediaPhoto: myImage });
+              function handleDragStart(event) {
+                function handleDrag(event) {
+                  const rect = container.getBoundingClientRect();
+
+                  draggable.style.transform = `translate(${event.clientX - rect.left}px, ${
+                    event.clientY - rect.top
+                  }px)`;
+                }
+
+                function handleDragEnd() {
+                  console.log({ rrrr: ref?.current?.getViewer()?.getConfig() });
+                  // Remove the event listeners when the drag ends
+                  document.removeEventListener("mousemove", handleDrag);
+                  document.removeEventListener("mouseup", handleDragEnd);
+                }
+
+                // Attach the event listeners for mousemove and mouseup to the document
+                document.addEventListener("mousemove", handleDrag);
+                document.addEventListener("mouseup", handleDragEnd);
+              }
+
+              handleDragStart(evt);
             }}
-          >
-            {" "}
-            enable{" "}
-          </button>
-          <div>
-            {this.state.test && (
-              <button onClick={this.handleClick}> disable </button>
-            )}
-          </div>
-        </div>
-        <h2 className="section_title">Image Component</h2>
-        <div className="pannellum_div">
-          <Pannellum
-            ref={this.ref}
-            width="800px"
-            height="400px"
-            image={this.state.mediaPhoto}
-            pitch={10}
-            yaw={this.state.yaww}
-            hfov={120}
-            autoLoad
-            author={this.state.author}
-            title=""
-          >
-            <Pannellum.Hotspot
-              type="info"
-              pitch={11}
-              yaw={-167}
-              text="Info Hotspot Text 3"
-              URL="https://github.com/farminf"
-            />
+            handleDragArg={{ name: "position-marker" }}
+          />
+        </Pannellum>
 
-            <Pannellum.Hotspot
-              type="custom"
-              pitch={31}
-              yaw={150}
-              handleClick={(evt, args) => this.hanldeClickImage(evt, args)}
-              handleClickArg={{ name: "test" }}
-            />
-          </Pannellum>
+        <div className="codebox">
+          <pre>
+            <code data-language="xml">
+              {`
+              <Pannellum
+                  width="800px"
+                  height="400px"
+                  image={this.state.mediaPhoto}
+                  pitch={10}
+                  yaw={180}
+                  hfov={500}
+                  autoLoad
+                  author=""
+                  title=""
+                  orientationOnByDefault={false}
+                  draggable
+                  keyboardZoom
+                  mouseZoom
+                  preview=""
+                  previewAuthor=""
+                  previewTitle=""
+                  showControls
+                  showFullscreenCtrl
+                  showZoomCtrl
+                  onLoad={()=>{console.log("panorama loaded");}}
+                  onScenechange={(id)=>{console.log("Scene has change on " + id);}}
+                  onScenechangefadedone={()=>{console.log("panorama loaded");}}
+                  onError={(err)=>{console.log("Error" , err);}}
+                  onErrorcleared={()=>{console.log("Error Cleared");}}
+                  onMousedown={(evt)=>{console.log("Mouse Down" , evt);}}
+                  onMouseup={(evt)=>{console.log("Mouse Up", evt);}}
+                  onTouchstart={(evt)=>{console.log("Touch Start", evt);}}
+                  onTouchend={(evt)=>{console.log("Touch End", evt);}}
+                  hotspotDebug={false}
+              >
+                  <Pannellum.Hotspot
+                    type="info"
+                    pitch={11}
+                    yaw={-167}
+                    text="Info Hotspot Text 3"
+                    URL="https://github.com/farminf"
+                  />
 
-          <div className="codebox">
-            <pre>
-              <code data-language="xml">
-                {`
-    <Pannellum
-        width="800px"
-        height="400px"
-        image={this.state.mediaPhoto}
-        pitch={10}
-        yaw={180}
-        hfov={500}
-        autoLoad
-        author=""
-        title=""
-        orientationOnByDefault={false}
-        draggable
-        keyboardZoom
-        mouseZoom
-        preview=""      
-        previewAuthor=""
-        previewTitle=""
-        showControls
-        showFullscreenCtrl
-        showZoomCtrl
-        onLoad={()=>{console.log("panorama loaded");}}
-        onScenechange={(id)=>{console.log("Scene has change on " + id);}}
-        onScenechangefadedone={()=>{console.log("panorama loaded");}}
-        onError={(err)=>{console.log("Error" , err);}}
-        onErrorcleared={()=>{console.log("Error Cleared");}}
-        onMousedown={(evt)=>{console.log("Mouse Down" , evt);}}
-        onMouseup={(evt)=>{console.log("Mouse Up", evt);}}
-        onTouchstart={(evt)=>{console.log("Touch Start", evt);}}
-        onTouchend={(evt)=>{console.log("Touch End", evt);}}
-        hotspotDebug={false}
-    >
-        <Pannellum.Hotspot 
-        type="info"
-        pitch={11}
-        yaw={-167}
-        text="Info Hotspot Text 3"
-        URL="https://github.com/farminf"
-        />
+                  <Pannellum.Hotspot
+                    type="custom"
+                    pitch={31}
+                    yaw={150}
+                    handleClick={(evt , args) => this.hanldeClickImage(evt , args)}
+                    handleClickArg={{ "name":"test" }}
+                  />
 
-        <Pannellum.Hotspot 
-        type="custom"
-        pitch={31}
-        yaw={150}
-        handleClick={(evt , args) => this.hanldeClickImage(evt , args)}
-        handleClickArg={{ "name":"test" }}
-        />
-
-    </Pannellum>
-                `}
-              </code>
-            </pre>
-          </div>
+              </Pannellum>
+            `}
+            </code>
+          </pre>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+// export default class ImageDemo extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       mediaPhoto: myImage,
+//       yaww: 180,
+//       test: false,
+//       updateText: "initial",
+//       author: "author",
+//     };
+//     this.ref = React.createRef();
+//   }
+
+//   hanldeClickImage = (evt, args) => {
+//     console.log(args.name);
+//     this.setState({
+//       mediaPhoto: myImage2,
+//     });
+//   };
+
+//   handleClick = () => {
+//     this.setState({
+//       mediaPhoto: myImage2,
+//       test: false,
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div className="image_main">
+//         <div style={{ display: "flex", flexDirection: "row" }}>
+//           <button onClick={() => this.setState((prevState) => ({ author: "change" }))}> change author </button>
+//           <button onClick={() => this.setState((prevState) => ({ yaww: prevState.yaww + 10 }))}> change yaw </button>
+//           <button onClick={() => this.setState({ updateText: "after update" })}> Should not update </button>
+//           <h3>{this.state.updateText}</h3>
+//           <button
+//             onClick={() => {
+//               this.setState({ test: true, yaww: 100, mediaPhoto: myImage });
+//             }}
+//           >
+//             {" "}
+//             enable{" "}
+//           </button>
+//           <div>{this.state.test && <button onClick={this.handleClick}> disable </button>}</div>
+//         </div>
+//         <h2 className="section_title">Image Component</h2>
+//         <div className="pannellum_div">
+//           <Pannellum
+//             ref={this.ref}
+//             width="800px"
+//             height="400px"
+//             image={this.state.mediaPhoto}
+//             pitch={10}
+//             yaw={this.state.yaww}
+//             hfov={120}
+//             autoLoad
+//             author={this.state.author}
+//             title=""
+//           >
+//             <Pannellum.Hotspot
+//               type="info"
+//               pitch={11}
+//               yaw={-167}
+//               text="Info Hotspot Text 3"
+//               URL="https://github.com/farminf"
+//             />
+
+//             <Pannellum.Hotspot
+//               yaw={120}
+//               pitch={212}
+//               type="custom"
+//               handleDrag={(evt, args) => {
+//                 const draggable = document.querySelector(".hotspot");
+
+//                 function handleDragStart(event) {
+//                   // Calculate the offset within the element where the click occurred
+//                   const offsetX = event.clientX - draggable.offsetLeft;
+//                   const offsetY = event.clientY - draggable.offsetTop;
+
+//                   function handleDrag(event) {
+//                     // Update the element's position based on the mouse movement
+//                     draggable.style.position = "absolute";
+//                     draggable.style.left = `${event.clientX - offsetX}px`;
+//                     draggable.style.top = `${event.clientY - offsetY}px`;
+//                   }
+
+//                   function handleDragEnd() {
+//                     console.log({ rrrr: this.ref });
+//                     // Remove the event listeners when the drag ends
+//                     document.removeEventListener("mousemove", handleDrag);
+//                     document.removeEventListener("mouseup", handleDragEnd);
+//                   }
+
+//                   // Attach the event listeners for mousemove and mouseup to the document
+//                   document.addEventListener("mousemove", handleDrag);
+//                   document.addEventListener("mouseup", handleDragEnd);
+//                 }
+
+//                 handleDragStart(evt);
+//               }}
+//               handleDragArg={{ name: "position-marker" }}
+//             />
+//           </Pannellum>
+
+//           <div className="codebox">
+//             <pre>
+//               <code data-language="xml">
+//                 {`
+//                   <Pannellum
+//                       width="800px"
+//                       height="400px"
+//                       image={this.state.mediaPhoto}
+//                       pitch={10}
+//                       yaw={180}
+//                       hfov={500}
+//                       autoLoad
+//                       author=""
+//                       title=""
+//                       orientationOnByDefault={false}
+//                       draggable
+//                       keyboardZoom
+//                       mouseZoom
+//                       preview=""
+//                       previewAuthor=""
+//                       previewTitle=""
+//                       showControls
+//                       showFullscreenCtrl
+//                       showZoomCtrl
+//                       onLoad={()=>{console.log("panorama loaded");}}
+//                       onScenechange={(id)=>{console.log("Scene has change on " + id);}}
+//                       onScenechangefadedone={()=>{console.log("panorama loaded");}}
+//                       onError={(err)=>{console.log("Error" , err);}}
+//                       onErrorcleared={()=>{console.log("Error Cleared");}}
+//                       onMousedown={(evt)=>{console.log("Mouse Down" , evt);}}
+//                       onMouseup={(evt)=>{console.log("Mouse Up", evt);}}
+//                       onTouchstart={(evt)=>{console.log("Touch Start", evt);}}
+//                       onTouchend={(evt)=>{console.log("Touch End", evt);}}
+//                       hotspotDebug={false}
+//                   >
+//                       <Pannellum.Hotspot
+//                         type="info"
+//                         pitch={11}
+//                         yaw={-167}
+//                         text="Info Hotspot Text 3"
+//                         URL="https://github.com/farminf"
+//                       />
+
+//                       <Pannellum.Hotspot
+//                         type="custom"
+//                         pitch={31}
+//                         yaw={150}
+//                         handleClick={(evt , args) => this.hanldeClickImage(evt , args)}
+//                         handleClickArg={{ "name":"test" }}
+//                       />
+
+//                   </Pannellum>
+//                 `}
+//               </code>
+//             </pre>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
--- src/elements/Pannellum.js
+++ src/elements/Pannellum.js
@@ -11,17 +11,12 @@ class Pannellum extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      id: Math.random()
-        .toString(36)
-        .substr(2, 9)
+      id: Math.random().toString(36).substr(2, 9),
     };
   }
 
   static propTypes = {
-    children: propTypes.oneOfType([
-      propTypes.arrayOf(propTypes.node),
-      propTypes.node
-    ]),
+    children: propTypes.oneOfType([propTypes.arrayOf(propTypes.node), propTypes.node]),
     id: propTypes.string,
     width: propTypes.string,
     height: propTypes.string,
@@ -52,7 +47,7 @@ class Pannellum extends PureComponent {
     keyboardZoom: propTypes.bool,
     mouseZoom: propTypes.bool,
     draggable: propTypes.bool,
-    disableKeyboardCtrl : propTypes.bool,
+    disableKeyboardCtrl: propTypes.bool,
     showFullscreenCtrl: propTypes.bool,
     showControls: propTypes.bool,
     onLoad: propTypes.func,
@@ -120,47 +115,36 @@ class Pannellum extends PureComponent {
     onRender: null,
   };
 
-  renderImage = state => {
+  renderImage = (state) => {
     const { children } = this.props;
     // make the array of sub components, even if its one, it become array of one
     let hotspots = [...children];
     let hotspotArray = [];
     if (Array.isArray(hotspots)) {
-      hotspots.map(hotspot => {
+      hotspots.map((hotspot) => {
         switch (hotspot.props.type) {
           case "info":
             return hotspotArray.push({
-              id: Math.random()
-                .toString(36)
-                .substr(2, 9),
+              id: Math.random().toString(36).substr(2, 9),
               type: hotspot.props.type,
               pitch: hotspot.props.pitch ? hotspot.props.pitch : 10,
               yaw: hotspot.props.yaw ? hotspot.props.yaw : 10,
               text: hotspot.props.text ? hotspot.props.text : "",
-              URL: hotspot.props.URL ? hotspot.props.URL : ""
+              URL: hotspot.props.URL ? hotspot.props.URL : "",
             });
           case "custom":
             return hotspotArray.push({
-              id: Math.random()
-                .toString(36)
-                .substr(2, 9),
+              draggable: hotspot.props.draggable ? hotspot.props.draggable : false,
+              id: Math.random().toString(36).substr(2, 9),
               pitch: hotspot.props.pitch ? hotspot.props.pitch : 10,
               yaw: hotspot.props.yaw ? hotspot.props.yaw : 10,
-              cssClass: hotspot.props.cssClass
-                ? hotspot.props.cssClass
-                : "tooltipcss",
-              createTooltipFunc: hotspot.props.tooltip
-                ? hotspot.props.tooltip
-                : this.hotspotTooltip,
-              createTooltipArgs: hotspot.props.tooltipArg
-                ? hotspot.props.tooltipArg
-                : {},
-              clickHandlerFunc: hotspot.props.handleClick
-                ? hotspot.props.handleClick
-                : this.handleClickHotspot,
-              clickHandlerArgs: hotspot.props.handleClickArg
-                ? hotspot.props.handleClickArg
-                : { name: "test" }
+              cssClass: hotspot.props.cssClass ? hotspot.props.cssClass : "tooltipcss",
+              createTooltipFunc: hotspot.props.tooltip ? hotspot.props.tooltip : this.hotspotTooltip,
+              createTooltipArgs: hotspot.props.tooltipArg ? hotspot.props.tooltipArg : {},
+              clickHandlerFunc: hotspot.props.handleClick ? hotspot.props.handleClick : this.handleClickHotspot,
+              clickHandlerArgs: hotspot.props.handleClickArg ? hotspot.props.handleClickArg : { name: "test" },
+              dragHandlerFunc: hotspot.props.handleDrag ? hotspot.props.handleDrag : this.handleDragHotspot,
+              dragHandlerArgs: hotspot.props.handleDragArg ? hotspot.props.handleDragArg : { name: "test" },
             });
           default:
             return [];
@@ -205,18 +189,13 @@ class Pannellum extends PureComponent {
       onRender: this.props.onRender,
     };
 
-    Object.keys(jsonConfig).forEach(
-      key => jsonConfig[key] === "" && delete jsonConfig[key]
-    );
+    Object.keys(jsonConfig).forEach((key) => jsonConfig[key] === "" && delete jsonConfig[key]);
     // this.setState({ jsonConfig });
 
     if (state === "update") {
       this.panorama.destroy();
     }
-    this.panorama = pannellum.viewer(
-      this.props.id ? this.props.id : this.state.id,
-      jsonConfig
-    );
+    this.panorama = pannellum.viewer(this.props.id ? this.props.id : this.state.id, jsonConfig);
 
     this.panorama.on("load", this.props.onLoad);
     this.panorama.on("scenechange", this.props.onScenechange);
@@ -227,7 +206,6 @@ class Pannellum extends PureComponent {
     this.panorama.on("mouseup", this.props.onMouseup);
     this.panorama.on("touchstart", this.props.onTouchstart);
     this.panorama.on("touchend", this.props.onTouchend);
-
   };
 
   componentDidMount = () => {
@@ -252,15 +230,17 @@ class Pannellum extends PureComponent {
     ) {
       this.renderImage("update");
     }
-    if (prevProps.maxYaw !== this.props.maxYaw ||
+    if (
+      prevProps.maxYaw !== this.props.maxYaw ||
       prevProps.minYaw !== this.props.minYaw ||
       prevProps.maxPitch !== this.props.maxPitch ||
       prevProps.minPitch !== this.props.minPitch ||
       prevProps.maxHfov !== this.props.maxHfov ||
-      prevProps.minHfov !== this.props.minHfov){
-      this.panorama.setYawBounds([this.props.minYaw,this.props.maxYaw]);
-      this.panorama.setPitchBounds([this.props.minPitch,this.props.maxPitch]);
-      this.panorama.setHfovBounds([this.props.minHfov,this.props.maxHfov]);
+      prevProps.minHfov !== this.props.minHfov
+    ) {
+      this.panorama.setYawBounds([this.props.minYaw, this.props.maxYaw]);
+      this.panorama.setPitchBounds([this.props.minPitch, this.props.maxPitch]);
+      this.panorama.setHfovBounds([this.props.minHfov, this.props.maxHfov]);
     }
     if (prevProps.yaw !== this.props.yaw) {
       this.panorama.setYaw(this.props.yaw);
@@ -275,6 +255,10 @@ class Pannellum extends PureComponent {
 
   handleClickHotspot = (e, args) => {
     console.log("hotspot clicked", args.name);
+  };
+
+  handleDragHotspot = (e, args) => {
+    console.log("hotspot dragged", args.name);
   };
 
   hotspotTooltip = (hotSpotDiv, args) => {
@@ -304,13 +288,13 @@ class Pannellum extends PureComponent {
     let { width, height } = this.props;
     let divStyle = {
       width: width,
-      height: height
+      height: height,
     };
     return (
       <div
         id={this.props.id ? this.props.id : this.state.id}
         style={divStyle}
-        ref={node => (this.imageNode = node)}
+        ref={(node) => (this.imageNode = node)}
       />
     );
   }
